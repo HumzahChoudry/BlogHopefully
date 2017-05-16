17aef61ee8e4266ae4527ecbd4263d8b612a894f
Christian Lang <clang2012@gmail.com>
2017-05-16 13:59:09 -0500

    Helping

--- BlogHopefully/Global.asax.cs
+++ BlogHopefully/Global.asax.cs
@@ -1,21 +1,35 @@
-﻿using System;
+﻿using Ninject.Web.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Ninject;
+using BlogHopefully.Core;
+using BlogHopefully.Core.Mappings;
 
 namespace BlogHopefully
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : NinjectHttpApplication
     {
-        protected void Application_Start()
+        protected new void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected override IKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+
+            kernel.Load(new RepositoryModule());
+            kernel.Bind<IBlogRepository>().To<BlogRepository>();
+
+            return kernel;
         }
     }
 }
